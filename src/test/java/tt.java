9bb25d7798078844b0993fdb54e0a1ac007700a1
zerorooot <msj1013654754@gmail.com>
2021-01-14 19:15:11 +0800

    新增规则

--- src/test/java/tt.java
+++ src/test/java/tt.java
@@ -8,9 +8,9 @@ import java.io.IOException;
  */
 public class tt {
     public static void main(String[] args) throws IOException {
-        String ip = "192.168.123.33";
-        int port = 4305;
-        String path = "D:\\JavaCode\\ZeroProxy\\src\\test\\config";
+        String ip = "192.168.123.34";
+        int port = 9999;
+        String path = "/home/ubuntu/Code/Java/ZeroProxy/src/test/config";
         String account = "admin";
         String password = "12345";
         System.out.println("http://"+ip+":"+port+"/index.html");
